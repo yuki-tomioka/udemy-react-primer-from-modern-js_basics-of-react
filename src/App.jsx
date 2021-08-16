59ce7af715c3ed853306295223876c393b89571d
Yuki Tomioka <yuki.tomioka.1016@gmail.com>
2021-08-16 18:21:40 +0900

    useEffectによる関心の分離

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => {
+  const [num, setNum] = useState(0); // [state名, 更新する関数] = usedState(初期値);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
+  const onClickCoutUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0); // [state名, 更新する関数] = usedState(初期値);
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
-      <button onClick={onClickButton}>カウントアップ！</button>
+      <button onClick={onClickCoutUp}>カウントアップ！</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(￣▽￣)</p>}
     </>
   );
 };
